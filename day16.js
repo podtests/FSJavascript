a5a5f93d8d19d6ba9ad5679ff21dec164cce321a
Akhil Jain <akhiljda@gmail.com>
2024-11-28 10:37:20 +0530

    changes

---
+++ day16.js
@@ -0,0 +1,44 @@
+//console.log(x);
+
+/*
+console.log(y);
+
+var x = 9;
+const y = 90;
+*/
+
+//Hoisting  : is the begaviour where you are able to use the variable before even declaring it.
+
+
+//9
+
+/*
+var x = 9;
+
+function calc() {
+    x++;
+}
+calc();
+
+console.log(x);
+*/
+
+
+
+
+//////
+let num1 = 90;
+console.log(num1);
+for(let i =0; i< 10; i++){
+    console.log("I am running the code")
+}
+
+
+
+//memory:                       //code of execution                         
+//num1: 90               // num1 = 90
+//i : undefined
+
+
+
+
