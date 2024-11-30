d49ff039e7f95d08dee4459d42c53b7644460667
H.S <40137734+techPioneeer@users.noreply.github.com>
2024-11-30 18:06:44 -0500

    day17 update

---
+++ day17_practise.js
@@ -0,0 +1,45 @@
+
+// console.log(num);
+// var num  =  9;
+
+// var a;
+// console.log(a);
+// a = 9;
+
+//  const a;
+
+// console.log(a);
+// let a = 89;
+
+
+// hello();
+// function hello(){
+//     console.log("Hello World");
+// }
+
+// l1();
+
+// const l1 = function () {
+//     console.log("Hello World")
+// }
+
+
+function f1(){
+    var a =  20;
+
+    function f2(){
+        console.log(a);
+    }
+    f2();
+}
+
+f1();
+
+
+
+
+
+
+
+
+
