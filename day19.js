f016cc4eede89274d19702e9d7a8565980989e5c
Akhil Jain <akhiljda@gmail.com>
2024-12-02 22:02:37 +0530

    changes

---
+++ day19.js
@@ -0,0 +1,38 @@
+//common JS Module:
+
+/*
+console.log("I am in module 1");
+
+exports.fisrtName = "Akhil Jain";
+exports.login = function () {
+    console.log("Login to the application!");
+}
+
+marks = [34, 78, 98];
+
+exports.age = 80;
+
+exports.addMarks = function(subjectMark) {
+    marks.push(subjectMark);
+}
+
+const printMarks = function() {
+    console.log(marks);
+}
+printMarks();
+*/
+
+
+// export 1 or multiple items
+//module.exports = {login}
+//module.exports = {fisrtName}
+
+
+//ESModules:
+function login() {
+    console.log("Login to the application!");
+}
+
+const marks = [34, 78, 98];
+
+export {login, marks}
---
+++ day19_1.js
@@ -0,0 +1,24 @@
+//commonJS
+
+//Testcase
+//const { } = require("./day19.js");
+
+
+//const obj =  {f1, fisrtName}
+/*
+printMarks();  //[34, 78, 98]
+
+addMarks(75);  //75
+printMarks();  //[34, 78, 98]  //[34, 78, 98, 75]
+//console.log(fisrtName);
+//login();
+
+console.log(age);  //80
+age++;
+console.log(age);  //81
+*/
+//ES Module imports:
+import {login, marks}  from "./day19.js"
+
+login();
+   
